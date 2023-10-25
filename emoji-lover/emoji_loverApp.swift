a1744a8625b56aa4400c9b006266394414cee3ec
Mohammad-Quanit <muhammadquanit@gmail.com>
2023-10-25 15:41:00 +0500

    Initial Commit

---
+++ emoji-lover/emoji_loverApp.swift
@@ -0,0 +1,17 @@
+//
+//  emoji_loverApp.swift
+//  emoji-lover
+//
+//  Created by mquanit on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct emoji_loverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
